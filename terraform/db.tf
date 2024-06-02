e3518c715345611c34792adc6fcecc15d7b30bd7
Samudrala Jeevana Lakshmi <113153018+samudralajeevanalakshmi@users.noreply.github.com>
2024-06-02 19:44:52 +0530

    Update db.tf

--- terraform/db.tf
+++ terraform/db.tf
@@ -20,7 +20,7 @@ resource "azurerm_mysql_server" "mysql" {
 }
 
 resource "azurerm_mysql_database" "main" {
-  name                = "revhiredb"
+  name                = "revhire"
   resource_group_name = azurerm_resource_group.main.name
   server_name         = azurerm_mysql_server.mysql.name
   charset             = "utf8"
